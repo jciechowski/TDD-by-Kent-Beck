80067dbed875ab181f2e7d9021fbb1b3dd7fab86
Jakub Ciechowski <sianex@gmail.com>
2016-01-10 15:34:44 +0100

    created repo

---
+++ FinanceManager/Money.cs
@@ -0,0 +1,45 @@
+﻿namespace FinanceManager
+{
+    public class Money
+    {
+        public int Amount { get; private set; }
+        private readonly string _currency;
+
+        protected Money(int amount, string currency)
+        {
+            Amount = amount;
+            _currency = currency;
+        }
+
+        public static Money Dollar(int amount)
+        {
+            return new Money(amount, "USD");
+        } 
+        
+        public static Money Franc(int amount)
+        {
+            return new Money(amount, "CHF");
+        }
+
+        public Money Times(int multiplier)
+        {
+            return new Money(Amount * multiplier, Currency());
+        }
+
+        public override bool Equals(object obj)
+        {
+            var money = (Money)obj;
+            return Amount == money.Amount && Currency().Equals(money.Currency());
+        }
+
+        public string Currency()
+        {
+            return _currency;
+        }
+
+        public Money Plus(Money addend)
+        {
+            return new Money(Amount + addend.Amount, Currency());
+        }
+    }
+}
---
+++ FinanceManagerSpecs/MultiplyingTests.cs
@@ -0,0 +1,60 @@
+﻿using FinanceManager;
+using Machine.Specifications;
+
+namespace FinanceManagerTests
+{
+    public class When_Multiplicating_Dollars
+    {
+        private It should_multiplie_value_by_2 = () => Money.Dollar(5).Times(2).Equals(Money.Dollar(10)).ShouldBeTrue();
+    }
+
+    public class When_Multiplicating_Dollar_Few_Times
+    {
+        Establish context = () =>
+        {
+            Five = Money.Dollar(5);
+        };
+
+        It should_give_10 = () => { Money.Dollar(10).Equals(Five.Times(2)).ShouldBeTrue(); };
+        It should_give_15 = () => { Money.Dollar(15).Equals(Five.Times(3)).ShouldBeTrue(); };
+        private static Money Five;
+    }
+
+    public class When_Multiplicating_Franc_Few_Times
+    {
+        Establish context = () =>
+        {
+            Five = Money.Franc(5);
+        };
+
+        It should_give_10 = () => { Money.Franc(10).Equals(Five.Times(2)).ShouldBeTrue(); };
+        It should_give_15 = () => { Money.Franc(15).Equals(Five.Times(3)).ShouldBeTrue(); };
+        private static Money Five;
+    }
+
+    public class When_Comparing_Money_Values
+    {
+        It should_return_true_for_same_values = () => { Money.Dollar(5).Equals(Money.Dollar(5)).ShouldBeTrue(); };
+        It should_return_false_for_different_values = () => { Money.Dollar(6).Equals(Money.Dollar(5)).ShouldBeFalse(); };
+    }
+
+
+    public class When_Comparing_Franc_To_Dollar
+    {
+        It should_do_the_math = () => { Money.Franc(5).Equals(Money.Dollar(5)).ShouldBeFalse(); };
+    }
+
+    public class When_Checking_Currency_Type
+    {
+        It should_return_USD_for_dollar = () => { Money.Dollar(1).Currency().ShouldEqual("USD"); };
+        It should_return_CHF_for_franc = () => { Money.Franc(1).Currency().ShouldEqual("CHF"); };
+    }
+
+    public class When_Adding_Money
+    {
+        Because of = () => { sum = Money.Dollar(5).Plus(Money.Dollar(5)); };
+
+        It should_add_5_dollars = () => { sum.Equals(Money.Dollar(10)).ShouldBeTrue(); };
+        private static Money sum;
+    }
+}
